a32d2e3d1bd9516549de8cec8e57776728ee28d3
CoderRx001 <carrillo964@gmail.com>
2017-10-09 20:12:15 -0500

    messenger 2 electric boogaloo

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -19,8 +19,10 @@ class ChatRoomsController < ApplicationController
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
+end
   end
-  
+
   private
 
   def chat_room_params
